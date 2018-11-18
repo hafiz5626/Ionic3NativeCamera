d3eec06e1b2b5a8fb299c2008d70bfd89c40f33e
Hafiz Mokhtar <hafiz5626@gmail.com>
2018-11-18 16:50:33 +0800

    Native camera functionalities
    - Added native camera functionalities

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { normalizeURL } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,66 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  imageData: any;
+
+  constructor(public navCtrl: NavController,
+    private camera: Camera) {
 
   }
 
+  takePictureFromCamera() {
+    let options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: 1,
+      cameraDirection: this.camera.Direction.BACK
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      console.log('imageData: ', imageData);
+      this.imageData = normalizeURL(imageData);
+      console.log('normaliza imageData: ', this.imageData);
+    }, (err) => {
+      console.log('error: ', err);
+    });
+  }
+
+  takeASefie() {
+    let options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: 1,
+      cameraDirection: this.camera.Direction.FRONT
+    }
+
+    this.camera.Direction.FRONT;
+    this.camera.getPicture(options).then((imageData) => {
+      console.log('imageData: ', imageData);
+      this.imageData = normalizeURL(imageData);
+      console.log('normaliza imageData: ', this.imageData);
+    }, (err) => {
+      console.log('error: ', err);
+    });
+  }
+
+  takePictureFromAlbum() {
+    let options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: 0
+    }
+    this.camera.getPicture(options).then((imageData) => {
+      console.log('imageData: ', imageData);
+      this.imageData = normalizeURL(imageData);
+      console.log('normaliza imageData: ', this.imageData);
+    }, (err) => {
+      console.log('error: ', err);
+    });
+  }
 }
